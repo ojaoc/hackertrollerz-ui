02d1ce539eb64b323e7979b3ac5b6cfed4f19fa5
ojaoc <correia.jafonso@gmail.com>
2020-04-28 16:30:24 +0100

    Updates

---
+++ precache-manifest.bce547c589def8b191ce0ef57d6aa6a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2b3363b6ecd8fa6e25061d82e6f992",
+    "url": "/hackertrollerz-ui/index.html"
+  },
+  {
+    "revision": "805cbbf367562f922679",
+    "url": "/hackertrollerz-ui/static/css/2.55ef3354.chunk.css"
+  },
+  {
+    "revision": "61a8146c5fff80686cb1",
+    "url": "/hackertrollerz-ui/static/css/main.66a3f015.chunk.css"
+  },
+  {
+    "revision": "805cbbf367562f922679",
+    "url": "/hackertrollerz-ui/static/js/2.2e328ced.chunk.js"
+  },
+  {
+    "revision": "0458999c2c367e463652c4bcc3d6311a",
+    "url": "/hackertrollerz-ui/static/js/2.2e328ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a8146c5fff80686cb1",
+    "url": "/hackertrollerz-ui/static/js/main.01e35073.chunk.js"
+  },
+  {
+    "revision": "8c2544d48c37699e6af4",
+    "url": "/hackertrollerz-ui/static/js/runtime-main.b7e44cf1.js"
+  }
+]);
